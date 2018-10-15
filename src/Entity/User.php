e734f03e063129e140176581804e62bb4459e3da
tiffvan <32732343+tiffvan@users.noreply.github.com>
2018-10-15 17:35:09 +0200

    User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,8 +5,76 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-class User
-{
+/**
+* @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+*/
 
+class UserInfo
+{
+    /**
+    * @ORM\Id()
+    * @ORM\GeneratedValue()
+    * @ORM\GeneratedValue(strategy="SEQUENCE")
+    * @ORM\Column(type="integer")
+    */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+    
+    /**
+    * @ORM\Column(type="string", length=255, nullable=true)
+    */
+    private $username;
+
+    /**
+    * @ORM\Column(type="string")
+    */
+    private $avatar;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getAvatar(): ?string
+    {
+        return $this->avatar;
+    }
+
+    public function setAvatar(string $avatar): self
+    {
+        $this->avatar = $avatar;
+
+        return $this;
+    }
 }
 ?>
