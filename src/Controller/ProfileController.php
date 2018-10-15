bfee71dbfd49b995a861d64b11f9934649a090e6
Caitie <caitieb@underware.co.za>
2018-10-15 21:41:05 +0200

    basic layout almost complete

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,5 +8,26 @@
     
     class ProfileController extends AbstractController 
     {
+        /**
+        * @Route("/login", name="login_view")
+        */
+        public function viewLogin()
+        {
+            
+            $view = 'login.html.twig';
+
+            return $this->render($view);
+        }
+
+        /**
+        * @Route("/signup", name="signup_view")
+        */
+        public function viewSignup()
+        {
+            
+            $view = 'register.html.twig';
+
+            return $this->render($view);
+        }
     }
 ?>
