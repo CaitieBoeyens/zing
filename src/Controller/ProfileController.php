072cffc22ceb509c294bebb6bbe7478d87099941
Caitie <caitieb@underware.co.za>
2018-10-15 22:28:58 +0200

    register form made with builder

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,8 +3,11 @@
     namespace App\Controller;
     
     use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\Routing\Annotation\Route;
+    use App\Entity\UserInfo;
+    use App\Form\UserInfoType;
     
     class ProfileController extends AbstractController 
     {
@@ -22,12 +25,46 @@
         /**
         * @Route("/signup", name="signup_view")
         */
-        public function viewSignup()
+        
+         public function newProfile(Request $request)
         {
+            $userInfo = new UserInfo();
+            $form = $this->createForm(UserInfoType::class, $userInfo);
             
-            $view = 'register.html.twig';
+            $form->handleRequest($request);
 
-            return $this->render($view);
+            if ($form->isSubmitted() && $form->isValid()) {
+                // $form->getData() holds the submitted values
+                $userProfile = $form->getData();
+
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($userInfo);
+                $entityManager->flush();
+
+
+                return $this->redirectToRoute('image_upload');
+            }
+
+            $view = 'register.html.twig';
+            $model = array('form' => $form->createView());
+
+            return $this->render($view, $model);
+        }
+
+        /**
+         * @Route ("signup/img", name="image_upload")
+         */
+        public function newAvatar(){}
+
+
+        /**
+        * @Route("/success", name="profile_success")
+        */
+        public function successProfile(Request $request)
+        {
+            $view = 'success.html.twig';
+            $model = array();
+            return $this->render($view, $model);
         }
     }
 ?>
