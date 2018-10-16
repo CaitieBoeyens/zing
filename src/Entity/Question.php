12b954d749a0aecd14c4621288711eeef35906eb
Brian-David <the.brian.irons@gmail.com>
2018-10-16 10:12:24 +0200

    Question, Response, User Relation

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -1,28 +1,92 @@
 <?php
-// src/Entity/Question.php
+
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ */
 class Question
 {
     /**
-     * @ORM\Column(type="string")
-     * @Assert\NotBlank()
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\UserInfo")
+     * @ORM\Column(type="integer")
+     */
+    private $user_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
      */
     private $question;
 
-    public function getQuestion()
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $rating;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $tags = [];
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserId(): ?int
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(int $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?string
     {
         return $this->question;
     }
 
-    public function setQuestion($question)
+    public function setQuestion(string $question): self
     {
         $this->question = $question;
 
         return $this;
     }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(?int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+
+    public function getTags(): ?array
+    {
+        return $this->tags;
+    }
+
+    public function setTags(array $tags): self
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
 }
-?>
---
+++ src/Entity/Response.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ResponseRepository")
+ */
+class Response
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $body;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Question")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $question_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): ?string
+    {
+        return $this->user_name;
+    }
+
+    public function setUserName(string $user_name): self
+    {
+        $this->user_name = $user_name;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getQuestionId(): ?Question
+    {
+        return $this->question_id;
+    }
+
+    public function setQuestionId(?Question $question_id): self
+    {
+        $this->question_id = $question_id;
+
+        return $this;
+    }
+}
