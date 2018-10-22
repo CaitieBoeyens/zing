43cd41c10c40cd49deb9be80d2b0337390939946
tiffvan <32732343+tiffvan@users.noreply.github.com>
2018-10-22 22:18:44 +0200

    migrate prob

--- src/Entity/UserProfile.php
+++ src/Entity/UserProfile.php
@@ -64,6 +64,20 @@ class UserProfile implements UserInterface
      */
     private $apiToken;
 
+    /**
+    * @ORM\Column(type="json")
+    */
+    private $roles = [];
+
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
     public function __construct()
     {
         $this->questionsAsked = new ArrayCollection();
