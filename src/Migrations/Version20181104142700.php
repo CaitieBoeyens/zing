2e9d43f887404d86aa2f299ab0bea59cc065c318
Caitie <caitieb@underware.co.za>
2018-11-05 16:56:10 +0200

    m

--- src/Migrations/Version20181104142700.php
+++ src/Migrations/Version20181104142700.php
@@ -16,7 +16,7 @@ final class Version20181104142700 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('DROP SEQUENCE login_id_seq CASCADE');
-        $this->addSql('CREATE SEQUENCE tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        /* $this->addSql('CREATE SEQUENCE tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1'); */
         $this->addSql('CREATE TABLE question_tag (question_id INT NOT NULL, tag_id INT NOT NULL, PRIMARY KEY(question_id, tag_id))');
         $this->addSql('CREATE INDEX IDX_339D56FB1E27F6BF ON question_tag (question_id)');
         $this->addSql('CREATE INDEX IDX_339D56FBBAD26311 ON question_tag (tag_id)');
