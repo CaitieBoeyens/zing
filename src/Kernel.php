0c7b2d8034635fdad5e3566840e2b550e2ce9095
Caitie <caitieb@underware.co.za>
2018-11-05 17:25:53 +0200

    kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -38,9 +38,6 @@ class Kernel extends BaseKernel
                     yield new $class();
                 }
             }
-            if (in_array($this->getEnvironment(), array('prod'))) {
-                $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-            }
         }
     }
 
